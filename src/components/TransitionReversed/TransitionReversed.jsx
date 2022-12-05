e590bb16c5b6296609ded7f36b362f064b90de60
Szymon Bosiak <szymon.bosiak.99@gmail.com>
2022-12-05 16:09:45 +0100

    Turend off some animations for better performance

--- src/components/TransitionReversed/TransitionReversed.jsx
+++ src/components/TransitionReversed/TransitionReversed.jsx
@@ -9,11 +9,11 @@ const TransitionReversed = () => {
               <defs>
                   <path id="gentle_wave_rev" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
               </defs>
-              <g className="parallax_rev">
-                  <use xlinkHref="#gentle_wave_rev" x="48" y="0" fill="rgba(255,255,255,0.7" />
-                  <use xlinkHref="#gentle_wave_rev" x="48" y="3" fill="rgba(255,255,255,0.5)" />
-                  <use xlinkHref="#gentle_wave_rev" x="48" y="5" fill="rgba(255,255,255,0.3)" />
-                  <use xlinkHref="#gentle_wave_rev" x="48" y="7" fill="#fff" />
+              <g className="parallax_rev--off">
+                  <use xlinkHref="#gentle_wave_rev" x="-79" y="7" fill="rgba(255,255,255,0.7" />
+                  <use xlinkHref="#gentle_wave_rev" x="20" y="9" fill="rgba(255,255,255,0.5)" />
+                  <use xlinkHref="#gentle_wave_rev" x="25" y="3" fill="rgba(255,255,255,0.3)" />
+                  <use xlinkHref="#gentle_wave_rev" x="-30" y="9" fill="#fff" />
               </g>
           </svg>
       </div>
